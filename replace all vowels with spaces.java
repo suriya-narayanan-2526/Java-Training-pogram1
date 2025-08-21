23c9992b54eb7701bed950cc8c05040c5860a30f
suriya-narayanan-2526 <suriyanarayananb22cs@psnacet.edu.in>
2025-08-21 21:42:47 +0530

    Create replace all vowels with spaces.java

---
+++ replace all vowels with spaces.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String replace = str.replaceAll("[AEIUOaeiou]"," ");
+        System.out.println(replace);
+    }
+}
