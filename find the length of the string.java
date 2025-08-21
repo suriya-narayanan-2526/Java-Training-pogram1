6299f82c729523915daea5c377d8373682330e1d
suriya-narayanan-2526 <suriyanarayananb22cs@psnacet.edu.in>
2025-08-21 21:41:52 +0530

    Create find the length of the string.java

---
+++ find the length of the string.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count =0;
+        for(int i=0 ;i<s.length();i++)
+        {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
