bd47cf5f07b99a6ae46f6ef9f05726e3cc062bf4
suriya-narayanan-2526 <suriyanarayananb22cs@psnacet.edu.in>
2025-08-29 16:09:22 +0530

    Create replace every element in the array with the sum of its every other element.java

---
+++ replace every element in the array with the sum of its every other element.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+                Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int sum=0;
+        int arr[] = new int[size];
+        for(int i=0 ;i<size ; i++)
+        {
+            arr[i]=sc.nextInt();
+            sum = sum+arr[i];
+        }
+        for(int i=0 ;i<size ;i++)
+        {
+            System.out.print(sum-arr[i]+" ");
+        }
+    }
+}
