77d542eaabdcb0cc7de06e204cd27bf58a2b8225
suriya-narayanan-2526 <suriyanarayananb22cs@psnacet.edu.in>
2025-09-05 14:30:19 +0530

    Create Xpattern.java

---
+++ Xpattern.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        n =n*2-1;
+        for(int r=1;r<=n;r++)
+        {
+            for(int c=1;c<=n;c++)
+            {
+                if(r==c || r+c==n+1 )
+                    System.out.print("*");
+                    else
+                        System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
